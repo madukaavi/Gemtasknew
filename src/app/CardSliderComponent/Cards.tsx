0b55bf0455a3a1a408ee52a58b29b60cf483e589
madukaavi <madukaavishka98@gmail.com>
2024-07-23 18:50:35 +0530

    fff

--- src/app/CardSliderComponent/Cards.tsx
+++ src/app/CardSliderComponent/Cards.tsx
@@ -10,7 +10,7 @@ interface CardsProps {
 }
 const Cards: FC<CardsProps> = ({ imageSrc, heading1, heading2, buttonText }) => {
   return (
-    <div className="relative w-[250px]  h-96 rounded-lg overflow-hidden shadow-lg flex flex-col bg-white">
+    <div className="relative w-[250px]  sm:w-[250px] sm:h-[230px]  h-96 rounded-lg overflow-hidden shadow-lg flex flex-col bg-white">
       <div className="absolute inset-0">
         <Image src={imageSrc} alt="Card Image" layout="fill" objectFit="cover" />
       </div>
