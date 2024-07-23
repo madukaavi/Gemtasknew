0bf11fa3f716accb88f3d571e837720d13f13df4
madukaavi <madukaavishka98@gmail.com>
2024-07-23 19:54:17 +0530

    ddd

--- src/app/CardSliderComponent/CardSlider.tsx
+++ src/app/CardSliderComponent/CardSlider.tsx
@@ -57,7 +57,7 @@ const CardSlider = () => {
           {cards.map((_, index) => (
             <button
               key={index}
-              className={`h-2 w-2 rounded-full ${index === currentIndex ? 'bg-blue-500' : 'bg-gray-300'}`}
+              className={`h-2 w-2 rounded-full ${index === currentIndex ? 'bg-customPink' : 'border-customPink'}`}
               onClick={() => handleDotClick(index)}
             />
           ))}
