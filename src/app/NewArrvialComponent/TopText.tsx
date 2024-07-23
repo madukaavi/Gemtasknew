e128f1ca222357ac1d61f1939893ff97edd27454
madukaavi <madukaavishka98@gmail.com>
2024-07-23 14:39:42 +0530

    fix new arrival text

--- src/app/NewArrvialComponent/TopText.tsx
+++ src/app/NewArrvialComponent/TopText.tsx
@@ -4,8 +4,8 @@ function TopText() {
   return (
     <>
     <div className="flex items-center justify-center flex-col">
-     <h2 className="font-sans font-semibold text-lg bg mb-3 text-pink-600">New Arrvials</h2>
-     <h1 className="font-sans font-bold md:text-4xl text-xl mb-3 ">Something New For You</h1>
+     <h2 className="font-inter font-semibold text-lg bg mb-3 relative top-1 text-custompinkpr">New Arrvials</h2>
+     <h1 className="font-inter font-bold md:text-4xl text-xl mb-3 ">Something New For You</h1>
     </div>
     </>
   )
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,7 +12,8 @@ const config: Config = {
         'custom-blue': 'rgb(220, 248, 239,0.4)',
         'custom-purple': 'rgb(254, 226, 248, 0.4)',
           'customPink': 'rgba(241, 39, 97, 1)',
-          'custompinknew':'rgba(116, 14, 76, 1)'
+          'custompinknew':'rgba(116, 14, 76, 1)',
+          'custompinkpr':'rgba(181, 2, 111, 1)'
       },
       fontFamily: {
         inter: ['Inter', 'sans-serif'],
