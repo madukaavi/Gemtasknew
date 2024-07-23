d02e202ad8b2b3c89cfac526aba2e81cfb78a098
madukaavi <madukaavishka98@gmail.com>
2024-07-23 14:16:10 +0530

    agin slove

--- src/app/CardSliderComponent/Cards.tsx
+++ src/app/CardSliderComponent/Cards.tsx
@@ -14,7 +14,7 @@ const Cards: FC<CardsProps> = ({ imageSrc, heading1, heading2, buttonText }) => 
       <div className="absolute inset-0">
         <Image src={imageSrc} alt="Card Image" layout="fill" objectFit="cover" />
       </div>
-      <div className="absolute inset-0 bg-white  flex flex-col justify-end p-4">
+      <div className="absolute inset-0 bg-black bg-opacity-30 flex flex-col justify-end p-4">
         <h2 className="text-md font-inter font-bold text-black">{heading1}</h2>
         <h3 className="text-xl font-inter font-bold text-black">{heading2}</h3>
         <button className="w-full bg-gradient-to-r from-pink-800 to-pink-400 text-white rounded-full py-2 mt-4 flex items-center justify-center">
