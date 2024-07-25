4baabf5ae42ad51cf49c5f5e7e12a829b991ef02
madukaavi <madukaavishka98@gmail.com>
2024-07-25 08:49:30 +0530

    fix g

--- src/app/FooterComponent/Footer.tsx
+++ src/app/FooterComponent/Footer.tsx
@@ -96,27 +96,27 @@ const Footer = () => {
                 <a href="#" className=" hover:translate-x-2 transition-transform duration-300 hover:text-customPink text-inter">Contact Us</a></li>
               <li className="flex items-center justify-center md:justify-start mb-1">
                 <IoMdArrowDropright  className="mr-2 text-lg md:text-xl" />
-                <a href="#" className=" hover:translate-x-2 transition-transform duration-300 hover:text-customPink text-inter">Support Center</a></li>
+                <a href="#" className=" hover:translate-x-2 transition-transform duration-300 hover:text-customPink text-inter">Support Center</a>
+              </li>
             </ul>
           </div>
           <div className="w-full md:w-1/2 lg:w-1/6 mb-6 text-center md:text-left">
-            <h3 className="font-bold text-lg mb-5 md:text-xl text-inter">App & Payment</h3>
-            <p className="text-base sm:text-md mb-4 text-inter font-semibold text-inter relative top-5 ">Download our Apps and get extra 15% Discount on your first Order…!</p>
-            <p className="text-base sm:text-md mb-4 text-inter font-semibold text-inter relative top-5">Secured Payment Gateways </p>
-            <div className="flex justify-center md:justify-start space-x-4 text-base md:text-lg relative top-5">
-              <a href="https://example.com/visa"><img className='h-[18px] w-[30px]' src="https://firebasestorage.googleapis.com/v0/b/gemgem-b7c15.appspot.com/o/logos_visaelectron.png?alt=media&token=579734ef-89ab-4df4-92ce-d41b873023d7" alt="Visa" /></a>
-              <a href="https://example.com/mastercard"><img className='h-[20px] w-[30px]'  src="https://firebasestorage.googleapis.com/v0/b/gemgem-b7c15.appspot.com/o/logos_mastercard.png?alt=media&token=ef95d40a-7e80-4367-8d56-91ff91f7bd1e" alt="Mastercard" /></a>
-              <a href="https://example.com/paypal"><img className='h-[20px] w-[30px]' src="https://firebasestorage.googleapis.com/v0/b/gemgem-b7c15.appspot.com/o/logos_amex.png?alt=media&token=248c7682-01bf-4179-b8ce-c80054823b44" alt="Paypal" /></a>
+            <h3 className="font-bold text-lg mb-5 md:text-xl text-inter">We Using Safe Payments</h3>
+            <div className="text-4xl md:text-5xl space-x-3">
+              <FaCcVisa className="inline text-customPink"/>
+              <FaCcMastercard className="inline text-customPink"/>
+              <FaCcPaypal className="inline text-customPink"/>
             </div>
           </div>
-          <div className="w-full  h-auto flex mobile-320:items-center  mobile-320:justify-center items-start justify-start relative bottom-5 gap-2 mobile-320:top-12 ">
-            <TiSocialFacebook size={35} className='text-blue-700' />
-            <FaGooglePlusG  size={35} className='text-blue-700'/>
-            <FaTwitter size={25}  className='text-blue-700 relative top-1 '/>
-            <FaPinterestP size={25}  className='text-blue-700  relative top-1'/>
-
-
+          <div className="w-full md:w-1/2 lg:w-1/6 mb-6 text-center md:text-left">
+            <h3 className="font-bold text-lg mb-5 md:text-xl text-inter">Social Media</h3>
+            <div className="flex space-x-3 justify-center md:justify-start text-4xl md:text-5xl">
+              <FaTwitter className="text-customPink cursor-pointer transition-transform duration-300 hover:scale-110" />
+              <FaPinterestP className="text-customPink cursor-pointer transition-transform duration-300 hover:scale-110" />
+              <FaGooglePlusG className="text-customPink cursor-pointer transition-transform duration-300 hover:scale-110" />
+              <TiSocialFacebook className="text-customPink cursor-pointer transition-transform duration-300 hover:scale-110" />
             </div>
+          </div>
         </div>
       </div>
     </footer>
